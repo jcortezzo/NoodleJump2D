e065f544b0aff0931ab2310d3ef63700f5c88f66
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-09-07 21:02:43 -0700

    jank noodle bounce

---
+++ NoodleJump2D/Assets/Scripts/AutoLevelGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoLevelGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject normalPlatformPrefab;
+    [SerializeField] private Player player;
+    [SerializeField] private float yOffset;
+
+    private Vector2 previousPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.y > previousPosition.y - (3 * 5))
+        {
+            GeneratePlatforms();
+        }
+    }
+
+    private void GeneratePlatforms()
+    {
+        int platforms = 10;
+        for(int i = 0; i < platforms; i++)
+        {
+            Vector2 position = new Vector2(Random.Range(-2, 2), previousPosition.y + yOffset);
+            Instantiate(normalPlatformPrefab, position, Quaternion.identity);
+            previousPosition = position;
+        }
+    }
+
+}
