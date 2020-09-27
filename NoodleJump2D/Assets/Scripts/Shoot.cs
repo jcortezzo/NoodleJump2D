b034ab00b56edd03f16fe427bbcfa00f7154326f
cortejon <cortejon@cs.washington.edu>
2020-09-27 16:47:53 -0700

    added bullets. Camera also corrected to behave more like Doodle Jump

---
+++ NoodleJump2D/Assets/Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] Bullet bullet;
+    [SerializeField] float bulletSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 dir = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            Bullet b = Instantiate<Bullet>(bullet, transform.position, Quaternion.identity);
+            b.Shoot(dir.normalized * bulletSpeed);
+        }
+    }
+}
